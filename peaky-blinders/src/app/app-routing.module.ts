c087095102a40aef6f66ed2d8af37a68a7dcf294
Lucas Ribeiro <lucas.ribeiro@unibenjf.local>
2024-09-03 14:37:08 -0300

    Terminando de montar a estrutura

--- peaky-blinders/src/app/app-routing.module.ts
+++ peaky-blinders/src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PaginaPrincipalComponent } from './paginas/pagina-principal/pagina-principal.component';
-import { SobreComponent } from './paginas/sobre/sobre.component';
-import { PersonagensComponent } from './paginas/personagens/personagens.component';
-import { TemporadasComponent } from './paginas/temporadas/temporadas.component';
+import { PaginaPrincipalComponent } from './pagina-principal/pagina-principal.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { PersonagensComponent } from './personagens/personagens.component';
+import { TemporadasComponent } from './temporadas/temporadas.component';
 
 const routes: Routes = [
-  { path: '', component: PaginaPrincipalComponent },
+  { path: '', redirectTo: '/pagina-principal', pathMatch: 'full' },
+  { path: 'pagina-principal', component: PaginaPrincipalComponent },
   { path: 'sobre', component: SobreComponent },
   { path: 'personagens', component: PersonagensComponent },
   { path: 'temporadas', component: TemporadasComponent },
--- peaky-blinders/src/app/app.module.ts
+++ peaky-blinders/src/app/app.module.ts
@@ -3,13 +3,10 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SobreComponent } from './paginas/sobre/sobre.component';
-import { PersonagensComponent } from './paginas/personagens/personagens.component';
-import { TemporadasComponent } from './paginas/temporadas/temporadas.component';
-import { PaginaPrincipalComponent } from './paginas/pagina-principal/pagina-principal.component';
-import { CabecalhoComponent } from './componente/cabecalho/cabecalho.component';
-import { RodapeComponent } from './componente/rodape/rodape.component';
-import { MenuOpcoesComponent } from './componente/menu-opcoes/menu-opcoes.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { PersonagensComponent } from './personagens/personagens.component';
+import { TemporadasComponent } from './temporadas/temporadas.component';
+import { PaginaPrincipalComponent } from './pagina-principal/pagina-principal.component';
 
 @NgModule({
   declarations: [
@@ -18,9 +15,6 @@ import { MenuOpcoesComponent } from './componente/menu-opcoes/menu-opcoes.compon
     PersonagensComponent,
     TemporadasComponent,
     PaginaPrincipalComponent,
-    CabecalhoComponent,
-    RodapeComponent,
-    MenuOpcoesComponent
   ],
   imports: [
     BrowserModule,
